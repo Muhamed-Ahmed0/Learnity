340f65a88c50731003532517535ed3eeb36e97cf
number 1 <number1@gmail.com>
2025-06-29 14:36:21 +0300

    implement navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Page = () => {
-  return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my saas app</h1>
-      <Button>تمام</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Page;
